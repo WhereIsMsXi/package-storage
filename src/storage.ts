20aba8339bff310fcd63ebd5b8cc1e88761c3699
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-21 17:25:53 +0800

    完成 session 的 get 方法

--- src/storage.ts
+++ src/storage.ts
@@ -1,10 +1,17 @@
+import { StorageType } from "./enum";
 
 const session = {
   set(name: string, data: unknown) {
-
+    const enrichContent = {
+      dataType: typeof data,
+      content: data,
+      type: StorageType.SESSION,
+      datatime: Math.floor(Date.now() / 1000),
+    };;
+    window.sessionStorage.setItem(name, JSON.stringify(enrichContent));
   },
   get(name: string) {
-
+    
   },
   del(name: string) {
     window.sessionStorage.removeItem(name);
