4175dec7020ce677f9764dd76e98f539410b7e3c
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-21 17:20:36 +0800

    定义框架

---
+++ src/storage.ts
@@ -0,0 +1,31 @@
+
+const session = {
+  set(name: string, data: unknown) {
+
+  },
+  get(name: string) {
+
+  },
+  del(name: string) {
+
+  },
+  clear() {
+
+  }
+};
+const local = {
+  set(name: string, data: unknown) {
+
+  },
+  get(name: string) {
+
+  },
+  del(name: string) {
+
+  },
+  clear() {
+
+  }
+};
+
+export { session, local };
